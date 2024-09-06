3508073e0b31f0b4da7524025e0f27b05034dc8d
QIZI94 <qizi94@gmail.com>
2024-09-06 22:06:05 +0200

    Assign new pinout to keyboard

--- keyboard.cpp
+++ keyboard.cpp
@@ -16,20 +16,28 @@ constexpr uint8_t KB_D9     = 9;
 
 
 static uint8_t KB_RowPins[] = {
-	KB_D5, KB_D4, KB_D3, KB_D2
+	Pinout::Assignment::KB_3,
+	Pinout::Assignment::KB_2,
+	Pinout::Assignment::KB_1,
+	Pinout::Assignment::KB_0
+	//KB_D5, KB_D4, KB_D3, KB_D2
 };
 constexpr uint8_t KB_RowCount = CONST_LENGTH(KB_RowPins);
 
 static uint8_t KB_ColumnPins[] = {
-	KB_D8, KB_D7, KB_D6	
+	Pinout::Assignment::KB_6,
+	Pinout::Assignment::KB_5,
+	Pinout::Assignment::KB_4,
+
+	//KB_D8, KB_D7, KB_D6	
 };
 constexpr uint8_t KB_ColumnCount = CONST_LENGTH(KB_ColumnPins);
 
 static char keyChars[KB_RowCount][KB_ColumnCount] = {
-    {'1','2','3'},
-    {'4','5','6'},
-    {'7','8','9'},
-    {'*','0','#'}
+    {'1','2','3',},
+    {'4','5','6',},
+    {'7','8','9',},
+    {'*','0','#',}
 };
 
 KeyboardModule::KeyboardModule(const char* moduleInstanceName, bool autoenable = true)
